cc1bd45913841d4d0be2146c22c24c94fbd6a7ce
akshitanitu <96866541+akshitanitu@users.noreply.github.com>
2022-02-12 16:01:51 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -17,10 +17,10 @@ function send(){
     LD=Math.floor(word.length/2);
     chart2=word.charAt(LD);
     LM=word.length-1;
-    chart3=word.charAt(lm);
+    chart3=word.charAt(LM);
     rc1=word.replace(chart1,"_");
-    rc2=word.replace(chart2,"_");
-    rc3=word.replace(chart3,"_");
+    rc2=rc1.replace(chart2,"_");
+    rc3=rc2.replace(chart3,"_");
     qw="<h4 id='wd'>Q."+rc3+"</h4>";
     ib="<br>ANSWER:<input type='text' id='icb'>";
     cb="<br><br><button class='btn btn-info' onclick='check()'>check</button>";
@@ -28,5 +28,42 @@ function send(){
 document.getElementById("output").innerHTML=row;
 document.getElementById("word").value="";
 }
+question_turn="player1";
+answe_turn="player2";
 
+function check(){
+get_anwer=document.getElementById("icb").value;
+answer=get_anwer.toLowerCase();
+if(answer==word)
+{if(answe_turn=="player1")
+{
+    player1_score=player1_score+1;
+    document.getElementById("player1_score").innerHTML=player1_score;
 
+}
+else{
+    player2_score=player2_score+1;
+    document.getElementById("player2_score").innerHTML=player2_score;
+}}
+if(question_turn=="player1")
+{
+    question_turn="player2";
+    document.getElementById("player_question").innerHTML="Question Turn -"+player2_name;
+
+}
+else{
+    question_turn="player1";
+    document.getElementById("player_question").innerHTML="Question Turn -"+player1_name;
+}
+if(answe_turn=="player1")
+{
+    answe_turn="player2";
+    document.getElementById("player_answer").innerHTML="Answer Turn -"+player2_name;
+
+}
+else{
+    answe_turn="player1";
+    document.getElementById("player_answer").innerHTML="Answer Turn -"+player1_name;
+}
+document.getElementById("output").innerHTML="";
+}
